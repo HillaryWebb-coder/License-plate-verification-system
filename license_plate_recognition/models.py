7adea524ef7b360f200f03eb4b8b0e973eac5e52
HillaryWebb <hillariosky123@gmail.com>
2021-09-08 00:43:28 +0100

    made changes

--- license_plate_recognition/models.py
+++ license_plate_recognition/models.py
@@ -13,7 +13,6 @@ class Profile(models.Model):
 
 class Log(models.Model):
 
-    user_profile = models.ForeignKey(Profile, verbose_name=_("User Details"), on_delete=models.CASCADE, blank=True, null=True)
     plate_number = models.CharField(_("Plate Number"), max_length=500, blank=True)
     offence = models.CharField(_("Offence"), max_length=500, blank=True, null=True)
     image = models.ImageField(_('Image'), upload_to='images/')
